f2a4cc855986411710f2489303826c9546b2bd52
bravejiawen <bravejiawen@outlook.com>
2017-08-03 00:08:48 +0800

    附加练习

--- ex4.py
+++ ex4.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+#将100赋值给cars
 cars = 100
 space_in_a_car = 4.0
 drivers = 30
 passengers = 90
+#将表达式cars-drivers 的值赋值给cars_not_driven
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
@@ -13,3 +16,7 @@ print "there will be", cars_not_driven, "empty cars today."
 print "we can transport", carpool_capacity, "people today."
 print "we have", passengers, "to carpool today."
 print "we need to put about",average_passagers_per_car, "in each car."
+
+#附加练习:表示未定义，car_pool_capacity拼写错误。
+#1，只用4结果会取整数
+#2，浮点数更精确
---
+++ ex5_f4.py
@@ -0,0 +1,11 @@
+ # -*- coding: utf-8 -*-
+my_name = 'jiawen'
+my_age = 25
+my_height = 66 #in
+my_weight = 150 #lb
+
+my_height_cm = my_height * 2.54
+my_weight_kg = my_weight / 2.2
+
+print "my height is %s cm. " % my_height_cm
+print "my weight is %s kg." % my_weight_kg
